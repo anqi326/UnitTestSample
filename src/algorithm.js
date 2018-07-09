48b5228117c4e9d5f6def8501080fd4290d042ce
An Qi <anqi11266128@sina.com>
2018-07-10 00:48:23 +0800

    add standard

--- src/algorithm.js
+++ src/algorithm.js
@@ -1,11 +1,11 @@
-export function square(x){
-  return x * x;
+export function square (x) {
+  return x * x
 }
 
-export function double(x){
-  return x*2;
+export function double (x) {
+  return x * 2
 }
 
-export function add(x,y){
-  return x + y;
+export function add (x, y) {
+  return x + y
 }
--- test/test.js
+++ test/test.js
@@ -1,38 +1,40 @@
+/* eslint-env mocha */
 import { square, double, add } from '../src/algorithm'
 import chai from 'chai'
-let expect = chai.expect;
+let expect = chai.expect
+// let it = Mocha.TestFunction
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return value when the value is present', function() {
-      expect([1,2,3].indexOf(2)).to.be.equal(1);
-    });
-    it('should return -1 when the value is not present', function() {
-      expect([1,2,3].indexOf(4)).to.be.equal(-1);
-    });
-  });
-});
+describe('Array', function () {
+  describe('#indexOf()', function () {
+    it('should return value when the value is present', function () {
+      expect([1, 2, 3].indexOf(2)).to.be.equal(1)
+    })
+    it('should return -1 when the value is not present', function () {
+      expect([1, 2, 3].indexOf(4)).to.be.equal(-1)
+    })
+  })
+})
 
-describe('Number', function() {
-  describe('#square()', function() {
-    it('should return squared value when value is a number', function() {
-      expect(square(2)).to.be.equal(4);
-    });
-    it('should return error when value is not number', function() {
-      expect(square('a')).to.be.NaN
-    });
-  });
-  describe('#double()', function() {
-    it('should return double value when value is a number', function() {
-      expect(double(2)).to.be.equal(4);
-    });
-    it('should return error when value is not number', function() {
-      expect(double('a')).to.be.NaN
-    });
-  });
-  describe('#add()', function() {
-    it('should return add value when value is a number', function() {
-      expect(add(2,3)).to.be.equal(5);
-    });
-  });
-});
+describe('Number', function () {
+  describe('#square()', function () {
+    it('should return squared value when value is a number', function () {
+      expect(square(2)).to.be.equal(4)
+    })
+    it('should return error when value is not number', function () {
+      expect(square('a')).to.be.NaN // eslint-disable-line
+    })
+  })
+  describe('#double()', function () {
+    it('should return double value when value is a number', function () {
+      expect(double(2)).to.be.equal(4)
+    })
+    it('should return error when value is not number', function () {
+      expect(double('a')).to.be.NaN // eslint-disable-line
+    })
+  })
+  describe('#add()', function () {
+    it('should return add value when value is a number', function () {
+      expect(add(2, 3)).to.be.equal(5)
+    })
+  })
+})
